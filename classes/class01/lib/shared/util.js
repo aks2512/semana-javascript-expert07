61e26606404cf80e6470055b0a8d03984e76467b
mzakira <jefferson.akira@mzgroup.com>
2023-03-01 04:52:01 -0300

    add class01

--- classes/class01/lib/shared/util.js
+++ classes/class01/lib/shared/util.js
@@ -1,29 +1,33 @@
-function supportsWorkerType() {
-  let supports = false 
-  const tester = {
-    get type() { supports = true}
-  }
-
-  try {
-    new Worker('blob://', tester).terminate()
-  } finally {
-    return supports
-  }
+function supportWorkerType() {
+    let supports = false;
+    const tester = {
+        get type() { supports = true } // it's supported
+    };
+    try {
+        //  We use "blob://" as url to avoid an useless network request.
+        // this will either throw in Chrome
+        // either fire an error event in Firework
+        // which is perfect since
+        // we don't need the worker to actually start,
+        // checking for the type of the script is done before trying to load it.
+        new Worker("blob://", tester).terminate();
+    } finally {
+        return supports;
+    }
 }
 
 function prepareRunChecker({ timerDelay }) {
-  let lastEvent = Date.now()
-  return {
-    shouldRun() {
-      const result = (Date.now() - lastEvent) > timerDelay
-      if(result) lastEvent = Date.now()
-
-      return result
+    let lastEvent = Date.now()
+    return {
+        shouldRun () {
+            const result = (Date.now() - lastEvent) > timerDelay;
+            if (result) lastEvent = Date.now()
+            return result;
+        }
     }
-  }
 }
 
 export {
-  supportsWorkerType,
-  prepareRunChecker
-}
+    supportWorkerType,
+    prepareRunChecker
+};
--- classes/class01/pages/titles/src/factories/cardsFactory.js
+++ classes/class01/pages/titles/src/factories/cardsFactory.js
@@ -1,6 +1,6 @@
 import CardsController from "./../controllers/cardsController.js"
-import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
+import CardsView from "./../views/cardsView.js"
 
 const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, { type: "module" })
 
@@ -8,11 +8,11 @@ const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
     return CardsController.initialize({
+      worker: cardListWorker,
       view: new CardsView(),
-      service: new CardsService({
+      service: new CardsService({ 
         dbUrl: `${rootPath}/assets/database.json`,
         cardListWorker
-
       })
     })
   }
